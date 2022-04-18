0cd4c1a14b2655ff8d4f6640fda5b53e6bb59431
MiRo1310 <michael.roling@gmx.de>
2022-04-18 20:13:32 +0200

    Erweiterung

--- javascript.js
+++ javascript.js
@@ -77,7 +77,50 @@ let valuesOb = {
         ist : 0,
         differenz: 0
 
-        }
+        },
+    zeiten:{
+        gesamtStunden:0,
+        tag1:{
+            start : "00:00",
+            ende : "00:00",
+            pause1 : 0,
+            pause2 : 0,
+            pause3 : 0,
+            pause4 : 0
+        },
+        tag2:{
+            start : "00:00",
+            ende : "00:00",
+            pause1 : 0,
+            pause2 : 0,
+            pause3 : 0,
+            pause4 : 0
+        },
+        tag3:{
+            start : "00:00",
+            ende : "00:00",
+            pause1 : 0,
+            pause2 : 0,
+            pause3 : 0,
+            pause4 : 0
+        },
+        tag4:{
+            start : "00:00",
+            ende : "00:00",
+            pause1 : 0,
+            pause2 : 0,
+            pause3 : 0,
+            pause4 : 0
+        },
+        tag5:{
+            start : "00:00",
+            ende : "00:00",
+            pause1 : 0,
+            pause2 : 0,
+            pause3 : 0,
+            pause4 : 0
+        },
+    }
     }
     
 
@@ -86,9 +129,8 @@ let values;
 
 // ANCHOR Reset Object
 // Local Storage zurück setzen
-if (false){
-    localStorage.setItem(storgekey, JSON.stringify(""))
-}
+//  if (true){localStorage.setItem(storgekey, JSON.stringify(""))}
+
 // Auf Änderungen reagieren
 let objectsOfChange = document.getElementsByClassName("input")
 for (const element of objectsOfChange){
@@ -121,8 +163,7 @@ addEventListener("load", function(){
     console.log(this.localStorage.getItem(storgekey))            
     if (this.localStorage.getItem(storgekey) && localStorage.getItem(storgekey) != '""' ){
         console.log("Loading")
-        
-        console.log(values)
+                
         values = JSON.parse(localStorage.getItem(storgekey));
         
         if (document.URL.includes("index.html")){
@@ -138,12 +179,12 @@ addEventListener("load", function(){
             }
         } else if (document.URL.includes("zaehlen.html")){
             refreshDisplayZaehlen();
-        }        
+        } else if (document.URL.includes("zeit.html")){
+            refreshDisplayZeit();
+        }               
     } else {
         console.log("First Value")
         values = valuesOb
-        refreshDisplay();
-        refreshDisplayZaehlen();
         save();
     }
     
@@ -232,9 +273,24 @@ if(zuruecksetzen){
         values.anfahrtPauschal.menge = 0;
         values.eurovignette.tage = 0;
         values.sonstiges.menge = 0;
-        save();
+        // save();
         refreshDisplay();
         berechnung();  
+    })
+}
+
+// ANCHOR Button Zurücksetzen Zählen
+const zuruecksetzenZaehlen = document.getElementById("zuruecksetzen2")
+if (zuruecksetzenZaehlen){
+    zuruecksetzenZaehlen.addEventListener("click", function(){
+        values.zaehlen.val.fuenf = 0
+        values.zaehlen.val.zehn = 0
+        values.zaehlen.val.zwanzig = 0
+        values.zaehlen.val.fuenfzig = 0
+        values.zaehlen.val.hundert = 0
+        values.zaehlen.val.zweihundert = 0
+        values.zaehlen.val.fuenfhundert = 0 
+        refreshDisplayZaehlen();
     })
 }
 
@@ -265,8 +321,9 @@ const euAusland = function(){
             }                        
             document.getElementsByClassName("button-feld")[0].style.bottom ="10px"    
         }
-        berechnung();
         save();
+        berechnung();
+        
 }
 
 // ANCHOR Zählen
@@ -298,6 +355,7 @@ for (const element of valuechange){
         wertErmitteln();
     })
 }
+
 
 // ANCHOR Refresh Display Zählen
 
@@ -342,14 +400,154 @@ function inputMult(){
 
 
 function gesamtErrechnen(){
- values.zaehlen.ist = ergFünf + ergZehn + ergZwanzig + ergFünfzig + ergHundert + ergZweiHundert + ergFünfHundert;
- gezaehlt.innerHTML = values.zaehlen.ist;
- values.zaehlen.differenz = values.zaehlen.soll - values.zaehlen.ist
- differenz.innerHTML = values.zaehlen.differenz.toFixed(2)
+    values.zaehlen.ist = ergFünf + ergZehn + ergZwanzig + ergFünfzig + ergHundert + ergZweiHundert + ergFünfHundert;
+    gezaehlt.innerHTML = values.zaehlen.ist;
+    values.zaehlen.differenz = values.zaehlen.soll - values.zaehlen.ist
+    differenz.innerHTML = values.zaehlen.differenz.toFixed(2)
+    style(parseFloat(values.zaehlen.differenz));
+}
+
+// ANCHOR Farbe Differenz Anpassen
+const style = function (val){
+    console.log("test")
+    console.log(typeof(val))
+
+    const dif = document.getElementById("dif")
+    if (val === 0){
+        console.log("ist gleich")
+        dif.style.backgroundColor = "#66ff33"
+    } else if (val != 0){
+        dif.style.backgroundColor ="red"
+    }
+    
+}
+
+// ANCHOR Übernahme Zeit 
+const buttonUebernahmeZeit = document.getElementById("uebernahmeStd")
+if (buttonUebernahmeZeit){
+    buttonUebernahmeZeit.addEventListener("click", function() {
+        zeit.value = values.zeiten.gesamtStunden
+        values.holzspalter.menge_holzspalter =  values.zeiten.gesamtStunden
+        save()
+        berechnung()
+    })
+}
+
+// ANCHOR Zeiten
+
+// ANCHOR Refresh Display Zeit
+const refreshDisplayZeit = function () {    
+    // Arbeitsbeginn
+    tB1.value = values.zeiten.tag1.start
+    tB2.value = values.zeiten.tag2.start
+    tB3.value = values.zeiten.tag3.start
+    tB4.value = values.zeiten.tag4.start
+    tB5.value = values.zeiten.tag5.start
+
+    // Arbeitsende
+    tE1.value = values.zeiten.tag1.ende
+    tE2.value = values.zeiten.tag2.ende
+    tE3.value = values.zeiten.tag3.ende
+    tE4.value = values.zeiten.tag4.ende
+    tE5.value = values.zeiten.tag5.ende
+
+    // Pausen
+    const tag1Pause = document.getElementsByClassName("t1P")
+    tag1Pause[0].value = values.zeiten.tag1.pause1
+    tag1Pause[1].value = values.zeiten.tag1.pause2
+    tag1Pause[2].value = values.zeiten.tag1.pause3
+    //tag1Pause[3].value = values.zeiten.tag1.pause4
+
+    const tag2Pause = document.getElementsByClassName("t2P")
+    tag2Pause[0].value = values.zeiten.tag2.pause1
+    tag2Pause[1].value = values.zeiten.tag2.pause2
+    tag2Pause[2].value = values.zeiten.tag2.pause3
+    //tag2Pause[3].value = values.zeiten.tag2.pause4
+
+    const tag3Pause = document.getElementsByClassName("t3P")
+    tag3Pause[0].value = values.zeiten.tag3.pause1
+    tag3Pause[1].value = values.zeiten.tag3.pause2
+    tag3Pause[2].value = values.zeiten.tag3.pause3
+    //tag3Pause[3].value = values.zeiten.tag3.pause4
+
+    const tag4Pause = document.getElementsByClassName("t4P")
+    tag4Pause[0].value = values.zeiten.tag4.pause1
+    tag4Pause[1].value = values.zeiten.tag4.pause2
+    tag4Pause[2].value = values.zeiten.tag4.pause3
+    //tag4Pause[3].value = values.zeiten.tag4.pause4
+
+    const tag5Pause = document.getElementsByClassName("t5P")
+    tag5Pause[0].value = values.zeiten.tag5.pause1
+    tag5Pause[1].value = values.zeiten.tag5.pause2
+    tag5Pause[2].value = values.zeiten.tag5.pause3
+    //tag5Pause[3].value = values.zeiten.tag5.pause4
+
+    mathTime()
 
 }
 
-// function datenAusAbrechnung(){
-//     soll.innerHTML = parseFloat(gesamtZaehlen).toFixed(2);
-    
-// }
+// ANCHOR Event Change Zeiten
+const fields = document.querySelectorAll(".field input")
+for(const input of fields){
+    input.addEventListener("change", function (){
+
+        console.log("Change")
+        const zeiten = values.zeiten;
+        for(let i = 1; i<=5; i++){
+            // Zeit Beginn          
+            const tB = document.getElementById("tB" + i)              
+            zeiten["tag" + i].start = tB.value;
+            // Zeit Ende
+            const tE = document.getElementById("tE" + i)
+            zeiten["tag" + i].ende = tE.value;
+      
+            const tag = document.getElementsByClassName("t" + i + "P")
+            values.zeiten["tag"+i].pause1 = tag[0].value
+            values.zeiten["tag"+i].pause2 = tag[1].value
+            values.zeiten["tag"+i].pause3 = tag[2].value
+            // values.zeiten["tag"+i].pause4 = tag[3].value   
+                       
+        }
+
+        save()
+        mathTime()
+    })
+}
+
+// ANCHOR Berechnen Zeit
+
+const mathTime = function (){
+    let gesamtStunden = 0;
+    for(let i = 1; i<=5; i++){
+
+        const tB = document.getElementById("tB" + i)
+        const tE = document.getElementById("tE" + i)
+        const pausen = document.getElementsByClassName("t" + i + "P")
+        
+        // Zeiten in Array Zerlegen
+        const timeBArray = (tB.value.split(":"))
+        const timeEArray = (tE.value.split(":"))
+
+        const timeB = new Date(00,00,00,timeBArray[0],timeBArray[1]).getTime()  
+        const timeE = new Date(00,00,00,timeEArray[0],timeEArray[1]).getTime()
+        
+        let zeitPause = 0
+        // Pausen zusammen addieren eines Tages
+        for (const pause of pausen){
+            zeitPause += parseFloat(pause.value)
+                    }
+        
+        if (timeE > timeB){
+            const arbeitszeit = (timeE - timeB)/60000 - zeitPause
+            const gesamtTag = document.getElementById("t" + i + "G")
+            gesamtTag.value = (arbeitszeit/60).toFixed(2);
+            gesamtStunden += parseFloat(arbeitszeit/60);
+            gesamtStd.innerHTML = gesamtStunden.toFixed(2)
+            values.zeiten.gesamtStunden = gesamtStunden.toFixed(2)
+
+        }
+        save()
+    }
+
+}
+
