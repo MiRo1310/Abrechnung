86c5eb575aaee0032ef08770d36f12cd3d15a86d
MiRo1310 <michael.roling@gmx.de>
2022-09-11 10:19:33 +0200

    installierbar

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,18 @@
 
 
+
 self.addEventListener('install', event => {
+    event.waitUntil(
+        addResourcesToCache([
+          "/",
+          "/index.html",
+          "/css/style.css",
+          "/app.js",
+          "/zaehlen.html",
+          "/zeit.html",
+          "/img/branding.png"
+        ])
+      );
 });
 self.addEventListener('fetch', event => {
 });
@@ -8,3 +20,7 @@ self.addEventListener('fetch', event => {
 self.addEventListener('activate', event => {
 });
 
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open("v1");
+    await cache.addAll(resources);
+  };
