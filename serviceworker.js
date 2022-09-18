d5beb89f17b98e2e06cd57a3bff2f88dc0dbef86
MiRo1310 <michael.roling@gmx.de>
2022-09-18 09:59:10 +0200

    First

--- serviceworker.js
+++ serviceworker.js
@@ -1,26 +1,110 @@
+let assets = [
+  "/",
+  "/zaehlen.html",
+  "/zeit.html",
+  "/css/style.css",
+  "/app.js"
+]
+
+let cacheTypes = ["main", "font", "image"];
+let cacheVersion = "_v1"
 
 
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        addResourcesToCache([
-          "/",
-          "/index.html",
-          "/css/style.css",
-          "/app.js",
-          "/zaehlen.html",
-          "/zeit.html",
-          "/img/branding.png"
-        ])
-      );
+  // Serviceworker aktivieren
+  self.skipWaiting();
+
+  // Erst weiter machen wenn der Code fertig ist
+  event.waitUntil(
+    // Cache öffnen , wenn nicht vorhanden wird er angelegt. Es wird ein Promise zurück gegeben
+    caches.open(cacheTypes[0] + cacheVersion).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+
 });
+
+function putInCache(request, response) {
+  let chacheKey = cacheTypes.includes(request.destination)
+    ? request.destination
+    : "main";
+  caches.open(chacheKey + cacheVersion).then((cache) => {
+    // put hinzufügen und ersetzen, add nur hinzufügen wenn nicht vorhanden
+    cache.put(request, response)
+  })
+}
+
+async function cacheFirst(request) {
+  // Abfrage ob der Request im Cache liegt
+  let responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+  // Wenn im Cache nichts gefunden wird, soll der Server angefragt werden
+  // Den Response vom fetch(request) gibt es nur einmal,und muss daher kopiert werden .clone() 
+  let responseFromNetwork = await (await fetch(request));
+  putInCache(request, responseFromNetwork.clone());
+  return responseFromNetwork;
+}
+
+async function networkFirst(request) {
+  try {
+    let responseFromNetwork = await fetch(request)
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+  }
+  catch {
+    let responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+      return responseFromCache;
+    }
+  }
+
+
+}
+
+
 self.addEventListener('fetch', event => {
+  //console.log(event.request)
+  let response = ""
+  switch (event.request.destination) {
+    case "font": response = cacheFirst(event.request);
+      break;
+    case "image": response = cacheFirst(event.request);
+      break;
+    default: response = networkFirst(event.request)
+  }
+
+  event.respondWith(response);
+
 });
+
+async function deleteOldCaches() {
+  let cacheKeepList = []
+  cacheTypes.forEach((element) => {
+    cacheKeepList.push(element + cacheVersion);
+  });
+  console.log("Cache To Keep : " + cacheKeepList) 
+
+  let keyList = await caches.keys();
+  console.log("Keylist: " + keyList)
+  let cacheToDelete = keyList.filter((key) => !cacheKeepList.includes(key))
+
+  console.log("Cache To Delete: " + cacheToDelete) 
+  await Promise.all(
+    cacheToDelete.map((key) => {
+      caches.delete(key)
+    })
+  )
+}
 
 self.addEventListener('activate', event => {
+  event.waitUntil(
+    deleteOldCaches().then(() => {
+      clients.claim();
+    })
+  );
+
 });
 
-const addResourcesToCache = async (resources) => {
-    const cache = await caches.open("v1");
-    await cache.addAll(resources);
-  };
